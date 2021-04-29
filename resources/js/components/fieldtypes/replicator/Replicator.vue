be28dacba49d1a6f5d8aea2e9d9cb1d1aa906dae
sauerbraten <willing.alexander@googlemail.com>
2021-04-29 12:06:03 +0200

    add and apply patch for TLP-1633
    (Replicator/Bard multiuser improvements)

--- resources/js/components/fieldtypes/replicator/Replicator.vue
+++ resources/js/components/fieldtypes/replicator/Replicator.vue
@@ -118,6 +118,14 @@ export default {
 
         storeState() {
             return this.$store.state.publish[this.storeName] || {};
+        },
+
+        lockedSets() {
+            return this.$store.state.publish[this.storeName].replicatorSetLocks[this.handle] || {};
+        },
+
+        canReorder() {
+            return !this.lockedSets || !Object.keys(this.lockedSets).length;
         }
     },
 
@@ -227,6 +235,7 @@ export default {
 
             return Object.keys(this.storeState.errors ?? []).some(handle => handle.startsWith(prefix));
         },
+
     },
 
     mounted() {
