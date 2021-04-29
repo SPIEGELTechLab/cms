4fa00353c804a03658cfd85c82c584c8aba0fd3a
sauerbraten <willing.alexander@googlemail.com>
2021-04-29 12:06:03 +0200

    add and apply patch for TLP-1633
    (Replicator/Bard multiuser improvements)

--- resources/js/components/publish/Sections.vue
+++ resources/js/components/publish/Sections.vue
@@ -59,8 +59,6 @@
                         @desynced="$emit('desynced', $event)"
                         @focus="$emit('focus', $event)"
                         @blur="$emit('blur', $event)"
-                        @focus-set="$emit('focus-set', $event)"
-                        @blur-set="$emit('blur-set', $event)"
                     />
                 </div>
             </div>
