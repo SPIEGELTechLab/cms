582e40da468047a623d6c6b73be802a7c174d7bc
sauerbraten <willing.alexander@googlemail.com>
2021-09-02 09:52:22 +0200

    add patch for TLP-2227
    properly disconnect echo/pusher to prevent automatic reconnect on tab close/reload

--- resources/js/components/Echo.js
+++ resources/js/components/Echo.js
@@ -31,7 +31,6 @@ class Echo {
         this.echo = new LaravelEcho(config);
         this.bootedCallbacks.forEach(callback => callback(this));
         this.bootedCallbacks = [];
-
         window.addEventListener('beforeunload', () => this.echo.disconnect());
     }
 }
