aab7f94e43dbb6cec33be0c0705de2e584ad94b7
sauerbraten <willing.alexander@googlemail.com>
2021-09-03 13:30:44 +0200

    improve TLP-2227.patch

--- resources/js/components/Echo.js
+++ resources/js/components/Echo.js
@@ -29,8 +29,6 @@ class Echo {
         };
 
         this.echo = new LaravelEcho(config);
-        window.addEventListener('unload', () => this.echo.disconnect());
-
         this.bootedCallbacks.forEach(callback => callback(this));
         this.bootedCallbacks = [];
 
