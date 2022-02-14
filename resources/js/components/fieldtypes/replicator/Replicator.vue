e1a92ea94dc0e3f9ebad8a921cd1a8c9cbe0a8ec
Wiebke Vogel <wiebke.vogel@spiegel.de>
2022-02-14 09:16:02 +0100

    removed duplicate code

--- resources/js/components/fieldtypes/replicator/Replicator.vue
+++ resources/js/components/fieldtypes/replicator/Replicator.vue
@@ -114,14 +114,6 @@ export default {
             return `${this.name}-sortable-handle`;
         },
 
-        lockedSets() {
-            return this.$store.state.publish[this.storeName].replicatorSetLocks[this.handle] || {};
-        },
-
-        canReorder() {
-            return !this.lockedSets || !Object.keys(this.lockedSets).length;
-        },
-
         storeState() {
             return this.$store.state.publish[this.storeName] || {};
         }
