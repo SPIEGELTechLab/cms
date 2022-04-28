89f82f1f5495ba2aebb85d9c1c7b0ba9ea5e4fe3
Wiebke Vogel <wiebke.vogel@spiegel.de>
2022-04-28 14:24:59 +0200

    Remove redirect patch

--- routes/cp.php
+++ routes/cp.php
@@ -63,6 +63,7 @@ Route::middleware('statamic.cp.authenticated')->group(function () {
 
             Route::group(['prefix' => '{entry}'], function () {
                 Route::get('/', 'EntriesController@edit')->name('collections.entries.edit');
+                Route::get('{slug}', fn ($collection, $entry, $slug) => redirect($entry->editUrl()));
                 Route::post('publish', 'PublishedEntriesController@store')->name('collections.entries.published.store');
                 Route::post('unpublish', 'PublishedEntriesController@destroy')->name('collections.entries.published.destroy');
                 Route::post('localize', 'LocalizeEntryController')->name('collections.entries.localize');
@@ -78,11 +79,6 @@ Route::middleware('statamic.cp.authenticated')->group(function () {
                 Route::patch('/', 'EntriesController@update')->name('collections.entries.update');
                 Route::get('{slug}', fn ($collection, $entry, $slug) => redirect($entry->editUrl()));
             });
-        });
-
-        // TODO: Should be removed at some point.
-        Route::get('{entry}/{slug}', function ($collection, $entry, $slug) {
-            return redirect($entry->editUrl(), 301);
         });
     });
 
