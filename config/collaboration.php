fa7b2416a205e47688c5cdde243e5340852c5427
Jonas Siewertsen <jonas.siewertsen@spiegel.de>
2022-06-30 12:02:26 +0200

    Add doc blocks to collaboration config

--- config/collaboration.php
+++ config/collaboration.php
@@ -18,15 +18,47 @@ return [
 
     'enabled' => env('STATAMIC_COLLABORATION_ENABLED', false),
 
+    /*
+    |--------------------------------------------------------------------------
+    | Default Provider
+    |--------------------------------------------------------------------------
+    |
+    | Collaboration does offer multiple providers to sync between clients.
+    |
+    | Peer to peer collaboration is the default, as it only requires one
+    | signaling server which syncs all changes directly between clients.
+    | This solution is great for smaller teams on normal web projects.
+    |
+    | The websocket connection does require a websocket server, which
+    | is in need of more resources to run. The actual state will be 
+    | saved on the websocket server directly and is a great option
+    | for sites with many users, as a websocket does scale well.
+    | 
+    | Available: `peer_to_peer` or `websocket`
+    |
+    */
+
     'default' => env('STATAMIC_COLLABORATION_PROVIDER', 'peer_to_peer'),
 
+    /*
+    |--------------------------------------------------------------------------
+    | Providers
+    |--------------------------------------------------------------------------
+    |
+    | Here are each of the provider connections setup for your application.
+    |
+    */
+
     'providers' => [
+
         'peer_to_peer' => [
             'url' => env('STATAMIC_COLLABORATION_URL', 'wss://signaling.yjs.dev'),
         ],
+
         'websocket' => [
             'url' => env('STATAMIC_COLLABORATION_URL', 'wss://demos.yjs.dev'),
-        ]
-    ]
+        ],
+
+    ],
 
 ];
