9bfbd7e1896cdbd2818fa989ce8f89caa7256b9d
Jonas Siewertsen <jonas.siewertsen@spiegel.de>
2022-08-02 12:10:49 +0200

    Add empty bard syncing-type

---
+++ resources/js/components/collaboration/syncing-types/Bard.js
@@ -0,0 +1,6 @@
+/**
+ * The Bard Fieldtype is synchronized with a custom implementation in the Fieldtype itself.
+ * 
+ * The implementation can be found in the individual fieldtypes:
+ * - BardFieldtype
+ */
