07232b47da803e5a3cc9bbb83cb624411cf68067
Wiebke Vogel <wiebke.vogel@spiegel.de>
2022-10-17 11:16:07 +0200

    Added patch for TLP-1923

--- resources/js/components/revision-history/Preview.vue
+++ resources/js/components/revision-history/Preview.vue
@@ -6,6 +6,7 @@ export default {
     mixins: [InlinePublishForm],
 
     props: {
+        indexUrl: String,
         revision: Object
     },
 
@@ -16,8 +17,16 @@ export default {
             method: 'patch',
             action: 'update',
             title: this.revision.attributes.data.title,
-            itemUrl: `${document.location.pathname}/revisions/${this.revision.id}`,
         }
+    },
+
+    computed: {
+        /* Polygon TLP-1923: Show history within an open stack */
+        itemUrl() {
+            return this.indexUrl
+                ? `${this.indexUrl}/${this.revision.id}`
+                : `${document.location.pathname}/revisions/${this.revision.id}`;
+        },
     },
 
     methods: {
