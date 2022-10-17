6dc74b3fc10069dd96c3c866182834fe0af3766a
Wiebke Vogel <wiebke.vogel@spiegel.de>
2022-10-17 13:50:40 +0200

    Added patch for TLP-1683

--- resources/lang/de/validation.php
+++ resources/lang/de/validation.php
@@ -87,6 +87,8 @@ return [
     'unique_entry_value' => 'Dieser Wert wurde bereits vergeben.',
     'unique_term_value' => 'Dieser Wert wurde bereits vergeben.',
     'unique_user_value' => 'Dieser Wert wurde bereits vergeben.',
+    /* Polygon TLP-1683: TODO - Check if we can solve the part from CMS */
+    'unique_value' => 'Dieser Wert wurde bereits vergeben.',
     'duplicate_field_handle' => 'Feld mit dem Handle :handle kann nicht mehr als einmal verwendet werden.',
     'one_site_without_origin' => 'Mindestens eine Website darf keine Quelle enthalten.',
     'origin_cannot_be_disabled' => 'Kann keine deaktivierte Quelle auswählen.',
