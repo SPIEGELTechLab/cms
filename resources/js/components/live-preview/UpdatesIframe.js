70838f5afee3ba8d84d1a2b3c1f3cb2514571845
Wiebke Vogel <wiebke.vogel@spiegel.de>
2023-03-28 09:30:02 +0200

    Added patch for TLP-3388

--- resources/js/components/live-preview/UpdatesIframe.js
+++ resources/js/components/live-preview/UpdatesIframe.js
@@ -22,6 +22,8 @@ export default {
     data() {
         return {
             previousUrl: null,
+            scrollX: 0,
+            scrollY: 0
         }
     },
 
@@ -57,10 +59,16 @@ export default {
 
             let isSameOrigin = url.startsWith('/') || iframeUrl.host === window.location.host;
             let preserveScroll = isSameOrigin && cleanUrl === this.previousUrl;
+            /* TLP-3388: With each reload, the last set position of the iFrame is located. 
+            However, the reload can be fired several times in a row (a user is typing slowly).
+            This means that the scrolling to the previously determined position has not yet taken 
+            place and the iFrame starts again at top: 0. */
+            this.scrollX = container.firstChild.contentWindow.scrollX > 0 ? container.firstChild.contentWindow.scrollX : this.scrollX;
+            this.scrollY = container.firstChild.contentWindow.scrollY > 0 ? container.firstChild.contentWindow.scrollY : this.scrollY;
 
-            let scroll = preserveScroll ? [
-                container.firstChild.contentWindow.scrollX ?? 0,
-                container.firstChild.contentWindow.scrollY ?? 0
+            let scroll = isSameOrigin ? [
+                this.scrollX,
+                this.scrollY
             ] : null;
 
             container.replaceChild(iframe, container.firstChild);
@@ -73,6 +81,18 @@ export default {
 
                 iframeContentWindow.addEventListener('DOMContentLoaded', iframeScrollUpdate, true);
                 iframeContentWindow.addEventListener('load', iframeScrollUpdate, true);
+                iframeContentWindow.addEventListener('scroll', this.resetScrollPosition, true);
+            }
+        },
+
+        // TLP-3388: To enable the user to start again at position 0 (line 29 + 30) the position must be reset at this point
+        resetScrollPosition() {
+            if (this.$refs.contents && this.$refs.contents.firstChild.contentWindow.scrollY === 0) {
+                this.scrollY = 0;
+            }
+
+            if (this.$refs.contents && this.$refs.contents.firstChild.contentWindow.scrollX === 0) {
+                this.scrollX = 0;
             }
 
             this.previousUrl = cleanUrl;
