9771539e03964e049f246f2551c0179e4f8f96b3
Wiebke Vogel <wiebke.vogel@spiegel.de>
2023-03-28 09:30:02 +0200

    Added patch for TLP-3388

--- resources/js/components/live-preview/UpdatesIframe.js
+++ resources/js/components/live-preview/UpdatesIframe.js
@@ -21,10 +21,7 @@ const postMessageToIframe = (container, url, payload) => {
 export default {
     data() {
         return {
-<<<<<<< HEAD
             previousUrl: null,
-=======
->>>>>>> 548f617ef (Added patch for TLP-3388)
             scrollX: 0,
             scrollY: 0
         }
